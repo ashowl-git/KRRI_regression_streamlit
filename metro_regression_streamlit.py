06556d5fdab8eb59d9672ebfdc768d6935b7f8ae
Ahn seung ho <ashaahn@gmail.com>
2023-02-04 06:52:19 +0900

    h

--- metro_regression_streamlit.py
+++ metro_regression_streamlit.py
@@ -204,8 +204,19 @@ st.subheader('에너지 사용량 예측값')
 st.caption('좌측의 변수항목 슬라이더 조정 ', unsafe_allow_html=False)
 st.caption('--------- ', unsafe_allow_html=False)
 # 예측값을 데이터 프레임으로 만들어 보기
+df_month = pd.read_excel('data/month.xlsx')
+
 df_result = pd.DataFrame(result, columns=lm_result_features).T.rename(columns={0:'kW'})
 df_result
+df_result.reset_index(inplace=True)
+df_result = pd.concat([df_result,df_month], axis=1)
+df_result
+
+
+
+
+
+
 
 
 # 예측값을 데이터 프레임으로 만들어본것을 그래프로 그려보기
