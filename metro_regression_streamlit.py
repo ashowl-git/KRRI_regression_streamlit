375991ab837cf0eaeece29520318149343677500
Ahn seung ho <ashaahn@gmail.com>
2023-02-04 20:03:07 +0900

    d

--- metro_regression_streamlit.py
+++ metro_regression_streamlit.py
@@ -68,7 +68,7 @@ page_names_to_funcs = {
 }
 
 selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
-# page_names_to_funcs[selected_page]()
+page_names_to_funcs[selected_page]()
 
 
 
