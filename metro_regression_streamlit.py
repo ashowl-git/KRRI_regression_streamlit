5c75b5e0ade723adf6dcc488a752a28f1ac13bb1
Ahn seung ho <ashaahn@gmail.com>
2023-02-04 21:32:31 +0900

    d

--- metro_regression_streamlit.py
+++ metro_regression_streamlit.py
@@ -150,8 +150,9 @@ lr2 = LinearRegression()
 lr.fit(X_train, y_train)
 lr2.fit(X_train2, y_train2)
 # 테스트 세트로 예측해보고 예측결과를 평가하기
+
 y_preds = lr.predict(X_test)
-y_preds2 = lr.predict(X_test2)
+y_preds2 = lr2.predict(X_test2)
 
 
 mse = mean_squared_error(y_test, y_preds)
