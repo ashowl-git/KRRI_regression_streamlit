a8cf94ca9572e0ede34f4be1cdd0183f27ad2b39
Ahn seung ho <ashaahn@gmail.com>
2023-02-05 18:25:13 +0900

    d

--- metro_regression_streamlit.py
+++ metro_regression_streamlit.py
@@ -398,11 +398,6 @@ fig = px.bar(df_result_merge, x='Month', y=['BASE_kW','ALT_kW'], title='ALT ',co
 fig
 
 
-fig = px.bar(dfdf, x='Month', y=['BASE_kW','ALT_kW'], title='ALT ',color='index' )
-# fig.update_xaxes(rangeslider_visible=True)
-fig.update_layout(barmode='group')
-fig
-
 
 
 
