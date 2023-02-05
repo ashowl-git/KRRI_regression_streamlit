3823998364db6b2aec8be37ded96ccf274b3c884
Ahn seung ho <ashaahn@gmail.com>
2023-02-05 18:11:12 +0900

    d

--- metro_regression_streamlit.py
+++ metro_regression_streamlit.py
@@ -337,12 +337,12 @@ st.caption('--------- ', unsafe_allow_html=False)
 fig = px.box(df_result_merge, x='index', y='BASE_kW', title='BASE', hover_data=['BASE_kW'], color='index' )
 fig.update_xaxes(rangeslider_visible=True)
 fig
-st.plotly_chart(fig, use_container_width=True)
+# st.plotly_chart(fig, use_container_width=True)
 
 fig = px.box(df_result_merge, x='index', y='ALT_kW', title='ALT', hover_data=['ALT_kW'],color='index' )
 fig.update_xaxes(rangeslider_visible=True)
 fig
-st.plotly_chart(fig, use_container_width=True)
+# st.plotly_chart(fig, use_container_width=True)
 
 
 # 예측값을 데이터 프레임으로 만들어본것을 그래프로 그려보기
@@ -352,11 +352,13 @@ st.caption('--------- ', unsafe_allow_html=False)
 
 fig = px.bar(df_result_merge, x='Month', y='BASE_kW', title='BASE ', hover_data=['BASE_kW'],color='index' )
 fig.update_xaxes(rangeslider_visible=True)
+fig
 st.plotly_chart(fig, use_container_width=True)
 
 fig = px.bar(df_result_merge, x='Month', y='ALT_kW', title='ALT ', hover_data=['ALT_kW'],color='index' )
 fig.update_xaxes(rangeslider_visible=True)
-st.plotly_chart(fig, use_container_width=True)
+fig
+# st.plotly_chart(fig, use_container_width=True)
 
 # 예측값을 데이터 프레임으로 만들어본것을 그래프로 그려보기
 
@@ -365,11 +367,13 @@ st.caption('--------- ', unsafe_allow_html=False)
 
 fig = px.line(df_result_merge, x='Month', y='BASE_kW', title='BASE ', hover_data=['BASE_kW'],color='index' )
 fig.update_xaxes(rangeslider_visible=True)
-st.plotly_chart(fig, use_container_width=True)
+fig
+# st.plotly_chart(fig, use_container_width=True)
 
 fig = px.line(df_result_merge, x='Month', y='ALT_kW', title='ALT ', hover_data=['ALT_kW'],color='index' )
 fig.update_xaxes(rangeslider_visible=True)
-st.plotly_chart(fig, use_container_width=True)
+fig
+# st.plotly_chart(fig, use_container_width=True)
 
 
 
