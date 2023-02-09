6609957f50e7092b20731715ff19c6057f4c5410
ashowl-git <ashaahn@gmail.com>
2023-02-10 08:05:27 +0900

    e

--- metro_regression_streamlit.py
+++ metro_regression_streamlit.py
@@ -496,7 +496,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 from obj2html import obj2html
 # 3D view
-html_string = obj2html(r"test.obj",height=500, width=800, html_elements_only=True)
+html_string = obj2html(r"test.obj", html_elements_only=True)
 components.html(html_string)
 # Download .obj button
 with open(r"test.obj") as f:
@@ -512,7 +512,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 from obj2html import obj2html
 # 3D view
-html_string = obj2html("test2.obj", html_elements_only=True)
+html_string = obj2html("test2.obj", height=500, width=800, html_elements_only=True)
 components.html(html_string)
 # Download .obj button
 with open("test2.obj") as f:
