9a5e50c4036b2ce06a9e6bcbd5568b06f4c71545
ashowl-git <ashaahn@gmail.com>
2023-02-10 08:06:16 +0900

    d

--- metro_regression_streamlit.py
+++ metro_regression_streamlit.py
@@ -512,11 +512,16 @@ import streamlit as st
 import streamlit.components.v1 as components
 from obj2html import obj2html
 # 3D view
-html_string = obj2html("test2.obj", height=500, width=800, html_elements_only=True)
-components.html(html_string)
+html_string = obj2html("test2.obj", html_elements_only=True)
+components.html(html_string, height=500, width=800)
 # Download .obj button
 with open("test2.obj") as f:
     st.download_button('Download model.obj', f, file_name="download_name.obj")
+
+
+
+
+
 
 
 import streamlit as st
