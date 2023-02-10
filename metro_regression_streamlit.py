30b6d8cab6a3133896014f6e16dc5f83687e38a3
ashowl-git <ashaahn@gmail.com>
2023-02-10 10:44:08 +0900

    e

--- metro_regression_streamlit.py
+++ metro_regression_streamlit.py
@@ -551,18 +551,18 @@ if st.button("Render in new window"):
 
 
 
-import streamlit as st
-import streamlit.components.v1 as components
-from obj2html import obj2html
+# import streamlit as st
+# import streamlit.components.v1 as components
+# from obj2html import obj2html
 
-st.header("3D Model Viewer 수정중")
-model_file = st.file_uploader("Upload your .obj file", type=['obj'])
+# st.header("3D Model Viewer 수정중")
+# model_file = st.file_uploader("Upload your .obj file", type=['obj'])
 
-if model_file is not None:
-    html_string = obj2html(model_file, html_elements_only=True)
-    components.html(html_string)
-    with open(model_file) as f:
-        st.download_button('Download model.obj', f, file_name="download_name.obj")
+# if model_file is not None:
+#     html_string = obj2html(model_file, html_elements_only=True)
+#     components.html(html_string)
+#     with open(model_file) as f:
+#         st.download_button('Download model.obj', f, file_name="download_name.obj")
 
 
 
