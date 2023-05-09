092188bd8685e569c30e72d83692d0e1422379e9
Ahn seung ho <ashaahn@gmail.com>
2023-05-09 20:56:24 +0900

    w

--- pages/pv+zeb (1).py
+++ pages/pv+zeb (1).py
@@ -151,14 +151,14 @@ st.subheader('에너지 사용량 예측값')
 st.caption('metro regression streamlit 불러온 값 ', unsafe_allow_html=False)
 
 
-# uploaded_file = st.file_uploader("Choose a file")
-# if uploaded_file is not None:
-#   DF4 = pd.read_csv(uploaded_file)
-#   st.write(DF4)
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+  DF4 = pd.read_csv(uploaded_file)
+  st.write(DF4)
 
 
 from metro_regression_streamlit import load_df
-DF4 = load_df()
+# DF4 = load_df()
 
 
 
