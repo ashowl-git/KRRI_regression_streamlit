f9e7e6d437c4effd557a1c36c248065091e1e24f
ashowl-git <ashaahn@gmail.com>
2023-05-09 13:06:27 +0900

    e

--- pages/pv+zeb.py
+++ pages/pv+zeb.py
@@ -150,12 +150,14 @@ A = DF6[제로에너지등급]
 st.subheader('에너지 사용량 예측값')
 st.caption('metro regression streamlit 불러온 값 ', unsafe_allow_html=False)
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-  DF4 = pd.read_csv(uploaded_file)
-  st.write(DF4)
+# uploaded_file = st.file_uploader("Choose a file")
+# if uploaded_file is not None:
+#   DF4 = pd.read_csv(uploaded_file)
+#   st.write(DF4)
 
 
+from metro_regression_streamlit import df_concat
+DF4 = df_concat
 # base 소요량 합계(보정계수 곱) = hh
 st.subheader('base 소요량 합계(보정계수 곱)')
 h = DF4.loc[(DF4['Alt'] == 'BASE')]
